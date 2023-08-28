f60a831bae5981233a0378571b880a43466af5b4
Guoxi Zhang <Altriaex86@gmail.com>
2023-08-28 10:33:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Quick fixes for D4RL
+Update D4Rl for recent Gymnasium apis.
+
+## Modifications
+1. Update to Gymnasium APIs
+2. Remove bullet environments
+3. Remove dependencies on mjrl
+
+## Setup
+
+```
+pip install git+https://github.com/Altriaex/d4rl.git
+```
+
+## Acknowledgements
+[Official Page for d4RL](https://github.com/Farama-Foundation/D4RL)
+
+
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='d4rl',
+    version='1.1',
+    install_requires=['gymnasium',
+                      'numpy',
+                      'mujoco',
+                      'h5py',
+                      'termcolor',  # adept_envs dependency
+                      'click',  # adept_envs dependency
+                      'dm_control'],
+    packages=find_packages(),
+    package_data={'d4rl': ['locomotion/assets/*',
+                           'hand_manipulation_suite/assets/*',
+                           'hand_manipulation_suite/Adroit/*',
+                           'hand_manipulation_suite/Adroit/gallery/*',
+                           'hand_manipulation_suite/Adroit/resources/*',
+                           'hand_manipulation_suite/Adroit/resources/meshes/*',
+                           'hand_manipulation_suite/Adroit/resources/textures/*',
+                           ]},
+    include_package_data=True,
+)
