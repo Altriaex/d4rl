0b93f89a15e984b5f7093ada071243609386ee14
Guoxi Zhang <altriaex86@gmail.com>
2023-08-29 19:14:12 +0800

    Disable Mujoco Ant envs due to the contact forces issue mentioned in https://gymnasium.farama.org/environments/mujoco/ant/

--- d4rl/gym_mujoco/gym_envs.py
+++ d4rl/gym_mujoco/gym_envs.py
@@ -1,15 +1,13 @@
+from mailbox import NotEmptyError
 from .. import offline_env
-#from gymnasium.envs.mujoco import HalfCheetahEnv, AntEnv, HopperEnv, Walker2dEnv
-#from gymnasium.envs.mujoco.ant_v5 import AntEnv
-#from gymnasium.envs.mujoco.half_cheetah_v5 import HalfCheetahEnv
-#from gymnasium.envs.mujoco.hopper_v5 import HopperEnv
-#from gymnasium.envs.mujoco.walker2d_v5 import Walker2dEnv
+
 from ..utils.wrappers import NormalizedBoxEnv
 import gymnasium as gym
 
 def get_ant_env(**kwargs):
-    env = gym.make("Ant-v5")
-    return NormalizedBoxEnv(offline_env.OfflineEnvWrapper(env, **kwargs))
+    #env = gym.make("Ant-v5", use_contact_forces=True)
+    #return NormalizedBoxEnv(offline_env.OfflineEnvWrapper(env, **kwargs))
+    raise NotEmptyError("There is a contact forces issue in the ant datasets of D4RL. See https://gymnasium.farama.org/environments/mujoco/ant/ for details.")
 
 def get_cheetah_env(**kwargs):
     env = gym.make("HalfCheetah-v5")
